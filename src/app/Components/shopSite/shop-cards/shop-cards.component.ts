559e50a090965e31ab3c25133adecd634a45faf8
X-Violet-X <bettercallginevra@gmail.com>
2024-04-04 21:16:41 +0200

    added sorting produtcs

--- src/app/Components/shopSite/shop-cards/shop-cards.component.ts
+++ src/app/Components/shopSite/shop-cards/shop-cards.component.ts
@@ -1,95 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'shop-cards',
   templateUrl: './shop-cards.component.html',
   styleUrls: ['./shop-cards.component.css']
 })
-export class ShopCardsComponent {
-  products: any[] = [
-    { 
-      name: "Prodotto 1",
-      price: 100,
-      image: "assets/1.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/2.jpg",
-      colors: ["#FB9AD1", "#BC7FCD","#86469C"]
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg",
-      colors: ["red", "blue"]
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
-    { 
-      name: "Prodotto 2",
-      price: 200,
-      image: "assets/3.jpg"
-    },
 
-  ];
+export class ShopCardsComponent implements OnInit  {
+  
+  //SORTING 
+  products: any[] = []; // Array per contenere i dati dei prodotti
+  originalProducts: any[] = []; // Array per contenere tutti i dati dei prodotti
+  colorDictionary: { [key: string]: string } = {
+    'Verde': '#008000', // Esempio di mappatura colore-verde
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient, private apiService: ApiService) { }
 
   ngOnInit(): void {
-
+    this.fetchProducts(); // Chiamata per ottenere i dati dei prodotti all'inizializzazione del componente
   }
+
+  fetchProducts(): void {
+    const url = 'http://localhost:5454/products/findByGenere/F'; // Url per ottenere i dati dei prodotti
+    this.apiService.apiGET(url).subscribe(
+      (response: any) => {
+        this.originalProducts = response; // Popola l'array originale di prodotti con i dati ricevuti dalla chiamata API
+        this.filterProducts(); // Chiama la funzione per filtrare i prodotti
+      },
+      (error: any) => {
+        console.log('Errore durante il recupero dei prodotti:', error);
+      }
+    );
+  }
+
+  filterProducts(): void {
+    const uniqueProductCodes = new Set(); // Utilizza un Set per memorizzare i codici prodotto unici
+    this.products = this.originalProducts.filter((product) => {
+      const firstFourLetters = product.codice_articolo.slice(0, 4); // Prendi solo le prime 4 lettere del codice prodotto
+      if (!uniqueProductCodes.has(firstFourLetters)) {
+        uniqueProductCodes.add(firstFourLetters);
+        return true; // Restituisci true se le prime 4 lettere del codice prodotto sono uniche
+      }
+      return false; // Restituisci false se le prime 4 lettere del codice prodotto sono duplicate
+    });
+  }
+
+  // products: any[] = []; // Array per contenere i dati dei prodotti
+
+  // constructor(private apiService: ApiService) { }
+
+  // ngOnInit(): void {
+  //   this.fetchProducts(); // Chiamata per ottenere i dati dei prodotti all'inizializzazione del componente
+  // }
+
+  // fetchProducts(): void {
+  //   const url = 'http://localhost:5454/products/getAllFe'; // Url per ottenere i dati dei prodotti
+  //   this.apiService.apiGET(url).subscribe(
+  //     (response: any) => {
+  //       this.products = response; // Popola l'array di prodotti con i dati ricevuti dalla chiamata API
+  //     },
+  //     (error: any) => {
+  //       console.log('Errore durante il recupero dei prodotti:', error);
+  //     }
+  //   );
+  // }
 }
